acd70f9f2ec811d44f3896c7600b71e79bf76ad5
Shiven Gupta <shivengupta@berkeley.edu>
2020-07-23 15:58:32 -0700

    Got stock data from sp500

--- getstockpricedata.py
+++ getstockpricedata.py
@@ -1,7 +1,6 @@
 import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
-from matplotlib.finance import candlestick_ohlc
 import matplotlib.dates as mdates
 import pandas as pd
 import pandas_datareader.data as web
