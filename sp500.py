9ab14db0581de4b541616974ed1aa794d349ad92
Shiven Gupta <shivengupta@berkeley.edu>
2020-07-26 03:00:10 -0700

    Plotted correlation

--- sp500.py
+++ sp500.py
@@ -1,5 +1,7 @@
 import bs4 as bs
 import datetime as dt 
+import matplotlib.pyplot as plt
+from matplotlib import style
 import os
 import pandas as pd 
 import pandas_datareader.data as web
@@ -7,6 +9,9 @@ from pandas_datareader import data as pdr
 import pickle
 import requests
 import yfinance as yf
+import seaborn as sns
+
+style.use('ggplot')
 
 yf.pdr_override()
 
@@ -42,5 +47,35 @@ def get_data_from_yahoo(reload_sp500 = False):
 		else:
 			print('Already have {}'.format(ticker))
 
-get_data_from_yahoo()
+def compile_data():
+	with open("sp500tickers.pickle", "rb") as f:
+		tickers = pickle.load(f)
+
+	main_df = pd.DataFrame()
+
+	for count, ticker in enumerate(tickers):
+		df = pd.read_csv('stock_dfs/{}.csv'.format(ticker))
+		df.set_index('Date', inplace = True)
+
+		df.rename(columns = {'Adj Close': ticker}, inplace = True)
+		df.drop(['Open', 'High', 'Low', 'Close', 'Volume'], 1, inplace = True)
+
+		if main_df.empty:
+			main_df = df
+		else:
+			main_df = main_df.join(df, how = 'outer')
+
+		if count % 10 == 0:
+			print(count)
+
+	print(main_df.head())
+	main_df.to_csv('sp500_joined_closes.csv')
+
+def visualize_data():
+	df = pd.read_csv('sp500_joined_closes.csv')
+	df_corr = df.corr()
+	sns.heatmap(df_corr, cmap="RdYlGn")
+	plt.show()
+
+visualize_data()
 
